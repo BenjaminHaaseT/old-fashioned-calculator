b333627be7faa5be97b09545b549a97d57c54bf7
Benjamin Haase <benjaminthaase@protonmail.com>
2023-08-20 18:33:03 -0700

    Need to add events to operation buttons, and everything is almost complete. Just need to find a way to deal with floating point numbers

--- scripts/app.js
+++ scripts/app.js
@@ -6,40 +6,47 @@ const acBtnElement = document.getElementById('clear-btn');
 const negBtnElement = document.getElementById('negate-btn');
 const modBtnElement = document.getElementById('mod-btn');
 const numberBtnsList = numberGridElement.children;
-// const sevenBtnElement = numberBtnsList[0];
-// const eightBtnElement = numberBtnsList[1];
-// const nineBtnElement = numberBtnsList[2];
-// const fourBtnElement = numberBtnsList[3];
-// const fiveBtnElement = numberBtnsList[4];
-// const sixBtnElement = numberBtnsList[5];
-// const oneBtnElement = numberBtnsList[6];
-// const twoBtnElement = numberBtnsList[7];
-// const threeBtnElement = numberBtnsList[8];
 
+const MOD = 'MOD';
+const ADD = 'ADD';
+const SUB = 'SUB';
+const DIV = 'DIV';
+const MUL = 'MUL';
 
-let currentValue = 0;
-let previousValue = 0;
+let currentValue = null;
+let previousValue = null;
 let mantissa = 0;
 let currentOp = null;
+
 
 for (let numbBtn of numberBtnsList) {
     const numbVal = parseInt(numbBtn.textContent);
     numbBtn.addEventListener('click', enterInput.bind(this, numbVal));
 }
 
-
 acBtnElement.addEventListener('click', clear);
-
+negBtnElement.addEventListener('click', negate);
+modBtnElement.addEventListener('click', setOperation.bind(this, MOD));
 
 function clear() {
     currentValue = 0;
     screenElement.value = currentValue;
 }
 
+function negate() {
+    if (screenElement.value) {
+        currentValue *= -1;
+        screenElement.value = currentValue;
+    }
+}
+
 function enterInput(input) {
+    // Check if the current operation has been set, if so move current value
+    // into previous value and set current value to input
     if (currentOp) {
         previousValue = currentValue;
-        currentValue = previousValue;
+        currentValue = input;
+        console.log(`Previous Value: ${previousValue}, current value: ${currentValue}, current operation: ${currentOp}`);
     } else {
         if (currentValue) {
             currentValue *= 10;
@@ -49,4 +56,30 @@ function enterInput(input) {
         }
     }
     screenElement.value = currentValue;
+}
+
+function setOperation(op) {
+    // Ensure that we have a valid current value to work with, otherwise do nothing
+    if (currentValue !== null) {
+        currentOp = op;
+    }
+}
+
+function performOperation() {
+    if (previousValue !== null && currentValue !== null && currentOp) {
+        if (currentOp === ADD)  {
+            currentValue += previousValue;
+        } else if (currentOp === SUB) {
+            currentValue = previousValue - currentValue;
+        } else if (currentOp === MUL) {
+            currentValue *= previousValue;
+        } else if (currentOp === DIV) {
+            currentValue = previousValue / currentValue;
+        } else if (currentOp === MOD) {
+            currentValue = previousValue % currentValue;
+        }
+        previousValue = null;
+        currentOp = null;
+        screenElement.value = currentValue;
+    }
 }
